fbb3edede569668cc9e5ff7bf460abd4a0674427
Nitesh <niteshgupta288@gmail.com>
2023-06-08 11:17:01 +0530

    Initial commit

---
+++ app/src/main/java/in/fivedegree/netclanclone/ExploreActivity.java
@@ -0,0 +1,17 @@
+package in.fivedegree.netclanclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/in/fivedegree/netclanclone/RefineActivity.java
@@ -0,0 +1,14 @@
+package in.fivedegree.netclanclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RefineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refine);
+    }
+}
